cdd8ff194ed565092c5b61e4de6fa75936dadf26
Kaloyan Ivanov <mesceto97@abv.bg>
2019-03-21 13:29:38 +0200

    Added recipes.xml and added two functions to the script file.

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 var data = [
+    //array first item
 	{
+        //object values
 		"id": 0,
 		"name": "kiufet",
 		"description": "blq blq desc",
 		"ingredients": [{"name": "sol", "amount": "100"}],
 		"instruction": "instrukcii"
 	},
+    //array second item
 	{
+        //object values
 		"id": 1,
 		"name": "supa",
 		"description": "supa desc",
@@ -14,9 +18,10 @@ var data = [
 		"instruction": "supa instrukcii"
 	}
 	];
-	
-function createCard(object){
-	return '<div class="card" id="recipe_' + object["id"] + '"><div class="image"></div>' +
+	    
+    
+function createCard(id, object){
+	return '<div class="card" id="recipe_' + id + '"><div class="image"></div>' +
 	'<div class="title">' + object["name"] + '</div></div>';
 };
 
@@ -32,6 +37,22 @@ fillCards(data);
 
 //TODO: id.split("_"), vzemam vtoriq string ot spisuka, pravq go na int i tursq
 //v obekta porednostta
+
+function fillRecipeCards(objects){
+    let cardString = "";
+    for(let i = 0; i < objects.length; i++){
+        let idString = objects[i]["id"];
+        let id = getObjectId(idString);
+        cardString += createCard(id, objects[i]);
+    }
+    $(".results").html(cardString);
+}
+
+function getObjectId(idString){
+    idString = idString.split("_");
+    let id = parseInt(idString[1]);
+    return id;
+}
 
 $(".card").click(function(){
 	$(".wrapper").addClass("visible");
