b1f517ade7e85c14802627fea333b8a6b783d76b
Bozhidar Stoyanov <bojidar.b.stoyanov@gmail.com>
2019-04-03 03:05:39 +0300

    Fixed auto-pull spamming warnings

--- auto-pull.py
+++ auto-pull.py
@@ -5,7 +5,7 @@ import random
 if __name__ == '__main__':
     while 1:
         print(f"[{time.ctime()}] Pulling...")
-        os.system("git checkout -b master")
+        os.system("git checkout master")
         os.system("git pull")
         os.system("git log --pretty --oneline -n5>git_latest.txt")
 
