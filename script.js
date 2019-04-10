9a815e1a79b2bf9955d2a0a12899a20f504e805a
Kaloyan Ivanov <mesceto97@abv.bg>
2019-04-11 01:40:13 +0300

    log log

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ function createCard(object){
 };
 
 function createBigCards(object){
+    console.log(object);
     let ingredients = "";
     for (let i of Object.entries(object)){
         ingredients += object["ingredients"];
