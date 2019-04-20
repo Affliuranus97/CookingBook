2370a8d2ded508fd143fde66130eaa97c4c25814
Kaloyan Ivanov <mesceto97@abv.bg>
2019-04-20 20:26:41 +0300

    Added spaces.

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ var reverseUnits = {
     Count: "бр"
 };
 
-String.prototype.replaceAt=function(index, replacement) {
+String.prototype.replaceAt = function(index, replacement) {
     return this.substr(0, index) + replacement + this.substr(index + replacement.length);
 }
     
