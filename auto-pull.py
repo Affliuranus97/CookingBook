956bf173e73305d6c620a2ff79778296e4035723
Bozhidar Stoyanov <bojidar.b.stoyanov@gmail.com>
2019-11-30 11:01:52 +0200

    Updated auto-pull to not be restricted to master branch.

--- auto-pull.py
+++ auto-pull.py
@@ -5,7 +5,6 @@ import random
 if __name__ == '__main__':
     while 1:
         print(f"[{time.ctime()}] Pulling...")
-        os.system("git checkout master")
         os.system("git pull")
         os.system("git log --pretty --oneline -n5>git_latest.txt")
 
