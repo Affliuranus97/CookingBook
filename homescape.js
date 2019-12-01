7d1d1e629fedd7beb017c8c3a93c2c685247b747
Kaloyan Ivanov <mesceto97@abv.bg>
2019-12-01 18:24:09 +0200

    Added a slider and fixed some display issues.

--- homescape.js
+++ homescape.js
@@ -1,3 +1,20 @@
 $(".searchList").click(function(){
-    $(this).find(".arrow").toggleClass("rotate-image");
+    $(this).find(".arrow").toggleClass("rotateImage");
+    if($("#priceButton").find(".arrow").hasClass("rotateImage")){
+        $("#hidden").removeClass("hidden");
+    }
+    else{
+        $("#hidden").addClass("hidden");
+    }
 });
+
+
+var rangeSlider = document.getElementById("slideBar");
+var rangeBullet = document.getElementById("priceValue");
+rangeBullet.innerHTML = rangeSlider.value;
+
+rangeSlider.oninput = function(){
+  rangeBullet.innerHTML = this.value;
+  var bulletPosition = (rangeSlider.value /rangeSlider.max);
+  rangeBullet.style.left = (bulletPosition * 278) + "px";
+};
