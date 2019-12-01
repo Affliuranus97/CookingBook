978b87339bd678b62260993dcdfce5a8b1292abc
Bozhidar Stoyanov <bojidar.b.stoyanov@gmail.com>
2019-12-02 01:01:20 +0200

    Search is working.

---
+++ data.js
@@ -0,0 +1,41 @@
+HS = {
+    Kinds: ["sale", "rent"],
+    Types: ["apartment", "house", "house floor"],
+    Constructions: ["panels", "bricks"],
+    RealEstate: [
+        {
+            kind: "sale",
+            city: "Varna",
+            district: "Levski",
+            type: "apartment",
+            price: 100000,
+            floor: 3,
+            floors: 6,
+            rooms: 3,
+            title: "",
+            size: 40,
+            year: 1999,
+            images: [
+                "https://q-cf.bstatic.com/images/hotel/max1024x768/134/134203664.jpg",
+            ],
+            construction: "panels",
+        },
+        {
+            kind: "rent",
+            city: "Varna",
+            district: "Vinitsa",
+            type: "apartment",
+            price: 400,
+            floor: 1,
+            year: 2004,
+            floors: 4,
+            rooms: 2,
+            title: "",
+            size: 36,
+            images: [
+                "https://q-cf.bstatic.com/images/hotel/max1024x768/134/134203664.jpg",
+            ],
+            construction: "bricks",
+        }
+    ]
+};
